e396f96c06d1356afdb3698b93f0589b38abe4b9
Henrik Hofmeister <henrik@newdawn.dk>
2023-11-15 21:37:26 +0100

    fix: Tests are pure TS

--- test/KapetaURI.test.ts
+++ test/KapetaURI.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from '@jest/globals';
-import { createKapetaUri, normalizeKapetaUri, parseKapetaUri } from '../src/KapetaURI.js';
+import { createKapetaUri, normalizeKapetaUri, parseKapetaUri } from '../src/KapetaURI';
 
 describe('KapetaURI', () => {
     test('Can parse kapeta uri', async () => {
--- test/Version.test.ts
+++ test/Version.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from '@jest/globals';
-import { createVersion, parseVersion } from '../src/Version.js';
+import { createVersion, parseVersion } from '../src/Version';
 
 describe('Version', () => {
     test('Can parse versions', async () => {
